f3044caf8a5e1cea9bb6b3097d8bfb29b9063291
suzanne su <suhysu@gmail.com>
2018-10-17 21:31:52 -0700

    done

--- LAB07-Collections/Card.cs
+++ LAB07-Collections/Card.cs
@@ -6,8 +6,8 @@ namespace LAB07_Collections
 {
     public class Card
     {
-        public string Value;
-        public Suits CardSuit;
+        public string Value { get; set; }
+        public Suits CardSuit { get; set; }
         public Card (string Val, Suits Suit)
         {
             Value = Val;
