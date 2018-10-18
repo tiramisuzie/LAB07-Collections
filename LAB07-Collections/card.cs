0394ff0dd3d44d8e79fa43a85a9cf7c7cdfc34a3
suzanne su <suhysu@gmail.com>
2018-10-17 21:30:09 -0700

    added get set

---
+++ LAB07-Collections/card.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LAB07_Collections
+{
+    public class Card
+    {
+        public string Value { get; set; }
+        public Suits CardSuit { get; set; }
+        public Card (string Val, Suits Suit)
+        {
+            Value = Val;
+            CardSuit = Suit;
+        }
+        public string ToString()
+        {
+            return Value + " " + CardSuit;
+        }
+    }
+
+   
+}
